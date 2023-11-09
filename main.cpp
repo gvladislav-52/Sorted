ae3317bd49f00b8a0b6ec307e916d638ad6a5daa
gvladislav-52 <gvladislav@mail.ru>
2023-11-09 18:33:42 +0300

    add Quck Sorter

--- main.cpp
+++ main.cpp
@@ -81,7 +81,43 @@ void smoothSort(int arr[], int length) {
     for (int i = 0; i < length; i++)
         cout << arr[i] << " ";
 }
+}
 
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high - 1];  // выбираем последний элемент в качестве опорного
+    int i = (low - 1);  // индекс меньшего элемента
+
+    for (int j = low; j < high - 1; j++) {
+        // если текущий элемент меньше опорного
+        if (arr[j] < pivot) {
+            i++;    // увеличиваем индекс меньшего элемента
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high - 1]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void start_and_show_quickSort(int arr[], int low, int high)
+{
+    quickSort(arr, low, high);
+    for (int i = 0; i < high; i++)
+        cout << arr[i] << " ";
 }
 
 
@@ -90,11 +126,13 @@ int main()
     const int LENGTH = 5;
     int arr[LENGTH] = {5,3,8,4,1};
     cout << "Sorter Buble: ";
-    bubbleSort(arr, LENGTH);
+    //bubbleSort(arr, LENGTH);
     cout << "\nSorter Select: ";
-    selectionSort(arr, LENGTH);
+    //selectionSort(arr, LENGTH);
     cout << "\nSorter Insert: ";
-    insertionSort(arr, LENGTH);
+    //insertionSort(arr, LENGTH);
     cout << "\nSorter Smooth: ";
-    smooth::smoothSort(arr,LENGTH);
+    //smooth::smoothSort(arr,LENGTH);
+    cout << "\nQuick Smooth: ";
+    start_and_show_quickSort(arr,0,LENGTH);
 }
